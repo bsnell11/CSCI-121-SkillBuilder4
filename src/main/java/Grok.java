b635e8bb5f0e33821280a0ea46130674b9e8bc07
briannasnell <brisnell11@gmail.com>
2026-03-05 23:38:25 -0500

    Added Grok class, methods, and constructors.

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -1,5 +1,5 @@
 
-/**
+/**  
  * Groks are bad actors in a game.  Groks have the ability to ingest
  * a PowerPill to replenish their energy.  This makes them difficult
  * to kill.
@@ -7,7 +7,35 @@
  * @author (You Again)
  * @version (0.1)
  */
-public class Grok
-{
-    // implement this class per instructions in Required Activities.
+
+// implement this class per instructions in Required Activities.
+public class Grok {
+    private static final int DEFAULT_POWER_LEVEL = 50;
+
+    private int powerLevel;
+
+    public Grok() {
+
+        powerLevel = DEFAULT_POWER_LEVEL;
+    }
+
+    public Grok(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    public int getPowerLevel(){
+        return powerLevel;
+}
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    public void takePowerPill(PowerPill pill){
+        powerLevel += pill.getPower();
+    }
+
+    public void tookHit(){
+        powerLevel -= 5;
+    }
 }
